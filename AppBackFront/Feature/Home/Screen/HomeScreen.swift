025debc3c9fba6a83047b7161dfd460a08d73eff
franco-ednaldo <ednaldofranco.dev@gmail.com>
2023-07-24 10:29:39 -0300

    Configured protocols

--- AppBackFront/Feature/Home/Screen/HomeScreen.swift
+++ AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -32,18 +32,17 @@ class HomeScreen: UIView {
         
         return search
     }()
-
+    
     lazy var collectionView:UICollectionView = {
-        let collectionView = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
         // TODO register
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
-        layout.scrollDirection = .horizontal
-        collectionView.setCollectionViewLayout(layout, animated: false)
         
         return collectionView
     }()
@@ -54,12 +53,28 @@ class HomeScreen: UIView {
         // TO DO: Register
         tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         tableView.separatorStyle = .none
+        
         return tableView
     }()
     
+    func configTableViewProtocol(delegate:UITableViewDelegate, dataSource:UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    func configCollectionViewProtocol(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = dataSource
+    }
+    
+    func configSearchBarDelegate(delegate: UISearchBarDelegate) {
+        searchBar.delegate = delegate
+    }
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         addElements()
         addConstraints()
     }
@@ -73,12 +88,35 @@ class HomeScreen: UIView {
         viewBackGround.addSubview(logoImageView)
         viewBackGround.addSubview(searchBar)
         viewBackGround.addSubview(collectionView)
-        
         addSubview(tableView)
-        
     }
     
     private  func addConstraints() {
-        
+        NSLayoutConstraint.activate([
+            viewBackGround.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            viewBackGround.leadingAnchor.constraint(equalTo: leadingAnchor),
+            viewBackGround.trailingAnchor.constraint(equalTo: trailingAnchor),
+            viewBackGround.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.topAnchor.constraint(equalTo: viewBackGround.topAnchor, constant: 5),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            logoImageView.heightAnchor.constraint(equalToConstant: 40),
+            logoImageView.widthAnchor.constraint(equalToConstant: 40),
+            
+            searchBar.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            searchBar.heightAnchor.constraint(equalToConstant: 46),
+            
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 15),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
 }
--- AppBackFront/Feature/Home/VC/HomeVC.swift
+++ AppBackFront/Feature/Home/VC/HomeVC.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    private var screen:HomeScreen?
+    override func loadView() {
+        let screen = HomeScreen()
+        view = screen
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        screen?.configTableViewProtocol(delegate: self, dataSource: self)
+        screen?.configCollectionViewProtocol(delegate: self, dataSource: self)
+        screen?.configSearchBarDelegate(delegate: self)
     }
     
 }
+
+extension HomeVC:UISearchBarDelegate {
+    
+}
+
+extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 150)
+    }
+    
+    
+}
+
+
+extension HomeVC:UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+
